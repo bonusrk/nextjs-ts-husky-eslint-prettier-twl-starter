8a9a98fe66c05a39e002d83cf8e5d608ffe90e99
bonusrk <word0bearer@gmail.com>
2022-01-27 10:25:55 +0300

    initial commit

---
+++ .eslintrc.js
@@ -0,0 +1,70 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['react', '@typescript-eslint', 'jest', 'import'],
+  extends: [
+    'airbnb-typescript',
+    'airbnb/hooks',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:jest/recommended',
+    'plugin:import/recommended',
+    'plugin:import/typescript',
+    'prettier',
+  ],
+  parserOptions: {
+    project: './tsconfig.eslint.json',
+  },
+  env: {
+    es6: true,
+    browser: true,
+    jest: true,
+    node: true,
+  },
+  settings: {
+    'import/resolver': {
+      typescript: {},
+    },
+    react: {
+      version: 'detect',
+    },
+  },
+  ignorePatterns: ['.eslintrc.js', '*.config.js'],
+  rules: {
+    'max-len': ['error', { code: 120 }],
+    'no-param-reassign': 0,
+    'import/prefer-default-export': 0,
+    'import/no-unresolved': [2, { caseSensitive: false }],
+    'react/no-array-index-key': 0,
+    'react/react-in-jsx-scope': 0,
+    'react/display-name': 0,
+    'react/prop-types': 0,
+    'react/jsx-props-no-spreading': 0,
+    'react/jsx-no-duplicate-props': ['error', { ignoreCase: false }],
+    'react/jsx-max-props-per-line': [1, { maximum: 2, when: 'multiline' }],
+    'react/jsx-newline': [2, { prevent: true }],
+    'jsx-a11y/no-noninteractive-element-interactions': 0,
+    'jsx-a11y/no-static-element-interactions': 0,
+    'jsx-a11y/click-events-have-key-events': 0,
+    'jsx-a11y/no-noninteractive-tabindex': 0,
+    '@typescript-eslint/naming-convention': 0,
+    '@typescript-eslint/explicit-function-return-type': 0,
+    '@typescript-eslint/explicit-member-accessibility': 0,
+    '@typescript-eslint/indent': 0,
+    '@typescript-eslint/member-delimiter-style': 0,
+    '@typescript-eslint/no-explicit-any': 0,
+    '@typescript-eslint/no-var-requires': 0,
+    '@typescript-eslint/no-use-before-define': 0,
+    '@typescript-eslint/no-unused-vars': [
+      2,
+      {
+        argsIgnorePattern: '^_',
+      },
+    ],
+    'no-underscore-dangle': 0,
+    'no-console': [
+      2,
+      {
+        allow: ['warn', 'error'],
+      },
+    ],
+  },
+};
